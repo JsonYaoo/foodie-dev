7d39eb9b70a368324608a594a273073b71b78b48
yaocs2 <yaocs2@midea.com>
2020-11-28 17:52:15 +0800

    foodie-dev> 商品列表搜索查询实现

--- foodie-dev-service/src/main/java/com/imooc/service/ItemService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/ItemService.java
@@ -53,17 +53,17 @@ public interface ItemService {
      */
     public PagedGridResult queryPagedComments(String itemId, Integer level,
                                               Integer page, Integer pageSize);
-//
-//    /**
-//     * 搜索商品列表
-//     * @param keywords
-//     * @param sort
-//     * @param page
-//     * @param pageSize
-//     * @return
-//     */
-//    public PagedGridResult searhItems(String keywords, String sort,
-//                                      Integer page, Integer pageSize);
+
+    /**
+     * 搜索商品列表
+     * @param keywords
+     * @param sort
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    public PagedGridResult searhItems(String keywords, String sort,
+                                      Integer page, Integer pageSize);
 //
 //    /**
 //     * 根据分类id搜索商品列表
