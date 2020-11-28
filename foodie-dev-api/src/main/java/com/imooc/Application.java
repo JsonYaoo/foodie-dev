140d2cefa2e34928c3194ddb8404ec28c1747dfa
yaocs2 <yaocs2@midea.com>
2020-11-28 11:45:43 +0800

    foodie-dev> CURD测试

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -2,8 +2,11 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+// 让mybatis去扫描mapper而不是spring去扫描, 注意要用tk包下的
+@MapperScan(basePackages = "com.imooc.mapper")
 public class Application {
 
     public static void main(String[] args) {
