bf44b938afcf2ec88788edeb63d3e9ecf44e60cc
yaocs2 <yaocs2@midea.com>
2020-11-29 15:25:51 +0800

    foodie-dev> 用户信息修改实现

--- foodie-dev-service/src/main/java/com/imooc/service/center/CenterUserService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/center/CenterUserService.java
@@ -1,6 +1,7 @@
 package com.imooc.service.center;
 
 import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.center.CenterUserBO;
 
 public interface CenterUserService {
 
@@ -10,13 +11,13 @@ public interface CenterUserService {
      * @return
      */
     public Users queryUserInfo(String userId);
-//
-//    /**
-//     * 修改用户信息
-//     * @param userId
-//     * @param centerUserBO
-//     */
-//    public Users updateUserInfo(String userId, CenterUserBO centerUserBO);
+
+    /**
+     * 修改用户信息
+     * @param userId
+     * @param centerUserBO
+     */
+    public Users updateUserInfo(String userId, CenterUserBO centerUserBO);
 //
 //    /**
 //     * 用户头像更新
