877c36775bda260efc6fd6b0ded69253e1063c64
yaocs2 <yaocs2@midea.com>
2020-11-29 14:30:03 +0800

    foodie-dev> 开启定时任务关闭未支付订单(不推荐)

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -3,6 +3,7 @@ package com.imooc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
@@ -10,6 +11,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(basePackages = "com.imooc.mapper")
 // 扫描SID所在包以及common层相关组件
 @ComponentScan(basePackages = {"com.imooc", "org.n3r.idworker"})
+// 开启定时任务
+//@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
