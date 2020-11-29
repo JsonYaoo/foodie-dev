ca6a2c44b79827f4b4cb498fe116fe1812e44fe9
yaocs2 <yaocs2@midea.com>
2020-11-29 10:38:30 +0800

    foodie-dev> 购物车商品添加 & 渲染

--- foodie-dev-mapper/src/main/java/com/imooc/mapper/ItemsMapperCustom.java
+++ foodie-dev-mapper/src/main/java/com/imooc/mapper/ItemsMapperCustom.java
@@ -2,6 +2,7 @@ package com.imooc.mapper;
 
 import com.imooc.pojo.vo.ItemCommentVO;
 import com.imooc.pojo.vo.SearchItemsVO;
+import com.imooc.pojo.vo.ShopcartVO;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -15,7 +16,7 @@ public interface ItemsMapperCustom {
 
     public List<SearchItemsVO> searchItemsByThirdCat(@Param("paramsMap") Map<String, Object> map);
 //
-//    public List<ShopcartVO> queryItemsBySpecIds(@Param("paramsList") List specIdsList);
+    public List<ShopcartVO> queryItemsBySpecIds(@Param("paramsList") List specIdsList);
 //
 //    public int decreaseItemSpecStock(@Param("specId") String specId,
 //                                     @Param("pendingCounts") int pendingCounts);
