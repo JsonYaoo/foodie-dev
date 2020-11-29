a2e2f37c279a5533b59949fbef6da870091124fe
yaocs2 <yaocs2@midea.com>
2020-11-29 10:58:45 +0800

    foodie-dev> 删除收货地址 & 设置默认收获地址 实现

--- foodie-dev-api/src/main/java/com/imooc/controller/AddressController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/AddressController.java
@@ -108,32 +108,32 @@ public class AddressController {
 
         return IMOOCJSONResult.ok();
     }
-//
-//    @ApiOperation(value = "用户删除地址", notes = "用户删除地址", httpMethod = "POST")
-//    @PostMapping("/delete")
-//    public IMOOCJSONResult delete(
-//            @RequestParam String userId,
-//            @RequestParam String addressId) {
-//
-//        if (StringUtils.isBlank(userId) || StringUtils.isBlank(addressId)) {
-//            return IMOOCJSONResult.errorMsg("");
-//        }
-//
-//        addressService.deleteUserAddress(userId, addressId);
-//        return IMOOCJSONResult.ok();
-//    }
-//
-//    @ApiOperation(value = "用户设置默认地址", notes = "用户设置默认地址", httpMethod = "POST")
-//    @PostMapping("/setDefalut")
-//    public IMOOCJSONResult setDefalut(
-//            @RequestParam String userId,
-//            @RequestParam String addressId) {
-//
-//        if (StringUtils.isBlank(userId) || StringUtils.isBlank(addressId)) {
-//            return IMOOCJSONResult.errorMsg("");
-//        }
-//
-//        addressService.updateUserAddressToBeDefault(userId, addressId);
-//        return IMOOCJSONResult.ok();
-//    }
+
+    @ApiOperation(value = "用户删除地址", notes = "用户删除地址", httpMethod = "POST")
+    @PostMapping("/delete")
+    public IMOOCJSONResult delete(
+            @RequestParam String userId,
+            @RequestParam String addressId) {
+
+        if (StringUtils.isBlank(userId) || StringUtils.isBlank(addressId)) {
+            return IMOOCJSONResult.errorMsg("");
+        }
+
+        addressService.deleteUserAddress(userId, addressId);
+        return IMOOCJSONResult.ok();
+    }
+
+    @ApiOperation(value = "用户设置默认地址", notes = "用户设置默认地址", httpMethod = "POST")
+    @PostMapping("/setDefalut")
+    public IMOOCJSONResult setDefalut(
+            @RequestParam String userId,
+            @RequestParam String addressId) {
+
+        if (StringUtils.isBlank(userId) || StringUtils.isBlank(addressId)) {
+            return IMOOCJSONResult.errorMsg("");
+        }
+
+        addressService.updateUserAddressToBeDefault(userId, addressId);
+        return IMOOCJSONResult.ok();
+    }
 }
