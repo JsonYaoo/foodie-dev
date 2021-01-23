079070d6e24e579ba419ad31df0fcd66b39eea19
yaocs2 <yaocs2@midea.com>
2021-01-23 18:25:54 +0800

    foodie-dev> ESTemplate实现删除索引

--- foodie-dev-search/src/main/java/com/imooc/es/pojo/Stu.java
+++ foodie-dev-search/src/main/java/com/imooc/es/pojo/Stu.java
@@ -3,8 +3,9 @@ package com.imooc.es.pojo;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
-@Document(indexName = "stu", type = "_doc")
+@Document(indexName = "stu", type = "_doc", shards = 3, replicas = 0)
 public class Stu {
 
     @Id
@@ -15,6 +16,15 @@ public class Stu {
 
     @Field(store = true)
     private Integer age;
+
+    @Field(store = true)
+    private Float money;
+
+    @Field(store = true, type = FieldType.Keyword)
+    private String sign;
+
+    @Field(store = true)
+    private String description;
 
     public Long getStuId() {
         return stuId;
@@ -38,5 +48,29 @@ public class Stu {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public Float getMoney() {
+        return money;
+    }
+
+    public void setMoney(Float money) {
+        this.money = money;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
--- foodie-dev-search/src/test/java/com/test/ESTest.java
+++ foodie-dev-search/src/test/java/com/test/ESTest.java
@@ -15,6 +15,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = EsApplication.class)
 public class ESTest {
 
+    /**
+     * 不建议使用 ElasticsearchTemplate 对索引进行管理（创建索引，更新映射，删除索引）
+     * 索引就像是数据库或者数据库中的表，我们平时是不会是通过java代码频繁的去创建修改删除数据库或者表的
+     * 我们只会针对数据做CRUD的操作
+     * 在es中也是同理，我们尽量使用 ElasticsearchTemplate 对文档数据做CRUD的操作
+     * 1. 属性（FieldType）类型不灵活
+     * 2. 主分片与副本分片数无法设置(该版本有bug)
+     */
     @Autowired
     private ElasticsearchTemplate esTemplate;
 
@@ -24,8 +32,16 @@ public class ESTest {
         stu.setStuId(1002L);
         stu.setName("spider man");
         stu.setAge(22);
+        stu.setMoney(18.8F);
+        stu.setSign("I am spider man");
+        stu.setDescription("I wish i am spider man");
 
         IndexQuery indexQuery = new IndexQueryBuilder().withObject(stu).build();
         esTemplate.index(indexQuery);
+    }
+
+    @Test
+    public void deleteIndexStu(){
+        esTemplate.deleteIndex(Stu.class);
     }
 }
