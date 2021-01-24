bebc76294ead70e6aca54b8af7ca873b77cc1574
yaocs2 <yaocs2@midea.com>
2021-01-24 14:14:58 +0800

    foodie-dev> search模块初始化Web环境

---
+++ foodie-dev-search/src/main/java/com/imooc/controller/ItemsController.java
@@ -0,0 +1,22 @@
+package com.imooc.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/esHello")
+public class ItemsController {
+
+    @GetMapping("/hello")
+    public Object hello(){
+        return "Hello ES~~~";
+    }
+
+}
